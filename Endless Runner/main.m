8ed6e5ecaac3a23f345fac1dd15c1caddaba38f7
SamTTaylor <sttaylor1@sheffield.ac.uk>
2014-11-25 18:15:46 +0000

    Base code from Lewin Lab

---
+++ Endless Runner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Endless Runner
+//
+//  Created by acp14stt on 25/11/2014.
+//  Copyright (c) 2014 sheffield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
