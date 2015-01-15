b7dc63ab3451a68fec29a3ef188faef9698e92a0
jenpaff <jparak1@sheffield.ac.uk>
2015-01-15 15:31:04 +0000

    bird, bog animation
    add animateSelf for birds
    add animateSelf for bog

--- Endless Runner/Bog.m
+++ Endless Runner/Bog.m
@@ -28,11 +28,22 @@
 
 - (void) animateSelf{
     [super animateSelf];
+    NSMutableArray *textures = [NSMutableArray arrayWithCapacity:16];
+    for (int i = 1; i < 7; i++) {
+        NSString *textureName = [NSString stringWithFormat:@"bog%d.png", i];
+        SKTexture *texture =[SKTexture textureWithImageNamed:textureName];
+        [textures addObject:texture];
+    }
+    SKTexture *texture =[SKTexture textureWithImageNamed:@"bog0.png"];
+    [textures addObject:texture];
+
     [self removeActionForKey:[NSString stringWithFormat:@"animate %@", self.class]];
     [self runAction:[SKAction sequence:@[[SKAction repeatAction:[SKAction sequence:@[[SKAction runBlock:^{
-        //Add Animation
+        //Add Bubbles
+        self.bubbleAnimation =[SKAction animateWithTextures:textures timePerFrame:3];
     }], [SKAction waitForDuration:1], [SKAction runBlock:^{
-        //Add Animation
+        //Add Bubbles
+        [self runAction:[SKAction repeatActionForever:self.bubbleAnimation]];
     }], [SKAction waitForDuration:1]]] count:5]]] withKey:[NSString stringWithFormat:@"animate %@", self.class]];
 }
 
