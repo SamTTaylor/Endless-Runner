324e8b444c2f181f9f51f564deff514b53ede4c9
jenpaff <jparak1@sheffield.ac.uk>
2015-01-21 11:10:22 +0000

    bird animation
    * add flyAnimation

--- Endless Runner/Bird.m
+++ Endless Runner/Bird.m
@@ -21,6 +21,7 @@
         self.physicsBody.contactTestBitMask = 0x1 << 1 | 0x1 << 3;
         self.physicsBody.categoryBitMask = 0x1 << 2;//enemy
         self.physicsBody.collisionBitMask = 0x1 << 1 | 0x1 << 3;
+        [self animateSelf];
     }
     return self;
 }
@@ -29,11 +30,23 @@
 //Animate flapping
 - (void) animateSelf{
     [super animateSelf];
+    
+    NSMutableArray *textures = [NSMutableArray arrayWithCapacity:16];
+    for (int i = 1; i < 3; i++) {
+        NSString *textureName = [NSString stringWithFormat:@"bird%d.png", i];
+        SKTexture *texture =[SKTexture textureWithImageNamed:textureName];
+        [textures addObject:texture];
+    }
+    SKTexture *texture =[SKTexture textureWithImageNamed:@"bird0.png"];
+    [textures addObject:texture];
+    
     [self removeActionForKey:[NSString stringWithFormat:@"animate %@", self.class]];
     [self runAction:[SKAction sequence:@[[SKAction repeatAction:[SKAction sequence:@[[SKAction runBlock:^{
-        //Add flap
+        //Add flyAnimation
+        self.flyAnimation = [SKAction animateWithTextures:textures timePerFrame:3];
     }], [SKAction waitForDuration:1], [SKAction runBlock:^{
-        //Add flap
+        //Add flyAnimation
+        [self runAction:[SKAction repeatActionForever:self.flyAnimation]];//repeat over and over again
     }], [SKAction waitForDuration:1]]] count:5]]] withKey:[NSString stringWithFormat:@"animate %@", self.class]];
 }
 
