1574629dab9ca8bb87bbbfd9bc2a3cdd1eb1dbcb
jenpaff <jparak1@sheffield.ac.uk>
2015-01-21 21:31:46 +0000

    create unit tests

---
+++ Endless RunnerTests/GameModelTest.m
@@ -0,0 +1,117 @@
+//
+//  GameModelTest.m
+//  Endless Runner
+//
+//  Created by acq14jp on 21/01/2015.
+//  Copyright (c) 2015 sheffield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "GameModel.h"
+
+@interface GameModelTest : XCTestCase
+
+@end
+
+@implementation GameModelTest
+
+-(void)testInitWithPlayer {
+    
+}
+
+-(void)testSaveAchievement{
+    
+}
+
+-(void)testIncrementScore {
+    
+}
+
+-(void)testIncrementDifficultyScore{
+    
+}
+
+-(void)testUpdateDifficulty{
+    
+}
+
+-(void)testUpdateLives{
+    
+}
+
+-(void)testRemoveLife{
+    
+}
+
+-(void)testMoveNodeWithGround{
+    
+}
+
+-(void)testFollowPlayer{
+    
+}
+
+-(void)testDressPlayer{
+    
+}
+
+-(void)testPlacePlayer{
+    
+}
+
+-(void)testPlaceEntWithLoc{
+    
+}
+
+-(void)testStopTactileObjectMovement{
+    
+}
+
+-(void)testMoveTactileObjectRight{
+    
+}
+
+-(void)testMoveTactileObjectLeft{
+    
+}
+
+-(void)testImpulseEntityRight{
+    
+}
+
+-(void)testImpulseEntityLeft{
+    
+}
+
+-(void)testJumpEntity{
+    
+}
+
+-(void)testSetFlying {
+    
+}
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ Endless RunnerTests/LivingEntityTest.m
@@ -0,0 +1,57 @@
+//
+//  LivingEntityTest.m
+//  Endless Runner
+//
+//  Created by acq14jp on 21/01/2015.
+//  Copyright (c) 2015 sheffield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "LivingEntity.h"
+
+@interface LivingEntityTest : XCTestCase
+
+@end
+
+@implementation LivingEntityTest
+
+-(void)testImpulseEntityRight{
+    
+}
+
+-(void)testImpulseEntityLeft{
+    
+}
+
+-(void)testJumpEntity{
+    
+}
+
+-(void)testSetFlying{
+    
+}
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
