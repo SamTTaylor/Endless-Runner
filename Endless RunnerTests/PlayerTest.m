db520c27dbac5cfcdd188be0bccd908c9bf6a753
jenpaff <jparak1@sheffield.ac.uk>
2015-01-22 20:08:20 +0000

    player unit test
    *test jump count
    *test invulnerable
    *test inMushroom
    *test inBog
    *testLives
    *testGotFollower
    *testAnimated
    *testCostumeArray

--- Endless RunnerTests/PlayerTest.m
+++ Endless RunnerTests/PlayerTest.m
@@ -9,22 +9,13 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 #import "Player.h"
+#import "GameModel.h"
 
-@interface PlayerTest : XCTestCase {
-    
-    int jumpcount;
-    bool inmushroom;
-    bool inbog;
-    int lives;
-    bool invulnerable;
-    SKAction *walkAnimation;
-    SKAction *jumpAnimation;
-    SKAction *injuredAnimation;
-    bool animated;
-    bool gotfollower;
-    CGPoint costumeposition;
-    NSMutableArray *costumearray;
-}
+@interface PlayerTest : XCTestCase
+
+@property Player *player;
+@property Entity *ent;
+@property GameModel *model;
 
 @end
 
@@ -33,27 +24,65 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.player = [[Player alloc] initWithTexture:self.ent.nodetexture];
 }
 
+//Jumpcount can be set to any Value
 - (void)testJumpCount{
-    XCTAssertEqual(jumpcount, 0);
+    [self.player setJumpcount:3];
+    XCTAssertEqual(self.player.jumpcount, 3);
+    //Reset JumpCount
+    [self.player resetJumpCount];
+    XCTAssertEqual(self.player.jumpcount, 0);
 }
 
+//Test if player runs into Mushroom
 - (void)testInMushroom{
-    XCTAssertFalse(inmushroom);
+    //Default is False
+    XCTAssertFalse(self.player.inmushroom);
+    //Set to true
+    [self.player setInmushroom:true];
+    XCTAssertTrue(self.player.inmushroom);
 }
 
+//Test if player runs into Bog
 - (void)testInBog{
-    XCTAssertFalse(inbog);
+    //Default is False
+    XCTAssertFalse(self.player.inbog);
+    //Set to true
+    [self.player setInmushroom:true];
+    XCTAssertTrue(self.player.inmushroom);
 }
 
+//Player starts with 3 lives
 - (void)testLives{
-    XCTAssertEqual(lives, 0);
+    XCTAssertEqual(self.player.lives, 3);
 }
 
+//Invulnerable is set to False
 - (void)testInvulnerable{
-    XCTAssertFalse(invulnerable);
+    XCTAssertFalse(self.player.invulnerable);
+    //Set to true
+    [self.player setInvulnerable:true];
+     XCTAssertTrue(self.player.invulnerable);
+    //If invulnerable is true, we can't remove lives
+    [self.model removeLife];
+    XCTAssertEqual(self.player.lives, 3);
+    
 }
+
+//Invulnerable is set to False
+- (void)testGotFollower{
+    XCTAssertFalse(self.player.gotfollower);
+    //Set to true
+    [self.player setGotfollower:true];
+    XCTAssertTrue(self.player.gotfollower);
+    //If Gotfollower is set to true true, we can't remove lives
+    [self.model removeLife];
+    XCTAssertEqual(self.player.lives, 3);
+    
+}
+
 
 - (void)testWalkAnimation{
     
@@ -67,21 +96,25 @@
     
 }
 
+//Player animation set to false by default
 - (void)testAnimated{
-     XCTAssertFalse(animated);
+     XCTAssertFalse(self.player.animated);
+    //start animating player
+    [self.player animateSelf];
+    XCTAssertTrue(self.player.animated);
+    //stop animating player
+    [self.player setAnimated:false];
+    XCTAssertFalse(self.player.animated);
 }
 
-- (void)testGotFollower{
-    XCTAssertFalse(gotfollower);
+
+- (void)testCostumeArray{
+    self.player.costumearray = [[NSMutableArray alloc] init];
+    XCTAssertNotNil(self.player.costumearray);
 }
 
 - (void)testCostumePosition{
     
-}
-
-- (void)testCostumeArray{
-    costumearray = [[NSMutableArray alloc] init];
-    XCTAssertNotNil(costumearray);
 }
 
 - (void)tearDown {
